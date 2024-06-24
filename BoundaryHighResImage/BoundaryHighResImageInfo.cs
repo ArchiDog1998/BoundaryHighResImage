347cc746c821b43ea66a8b8b8838f755366e43ce
秋水 <53346444+ArchiDog1998@users.noreply.github.com>
2024-06-24 20:24:02 +0800

    chore: 1.0.0 fix...

--- BoundaryHighResImage/BoundaryHighResImageInfo.cs
+++ BoundaryHighResImage/BoundaryHighResImageInfo.cs
@@ -14,15 +14,15 @@ public class BoundaryHighResImageInfo : GH_AssemblyInfo
     public override Bitmap Icon => null;
 
     //Return a short string describing the purpose of this GHA library.
-    public override string Description => "";
+    public override string Description => "Create a hi-res image asap.";
 
     public override Guid Id => new ("2c5f0c76-645b-4037-a4cb-79d100f2e4be");
 
     //Return a string identifying you or your company.
-    public override string AuthorName => "";
+    public override string AuthorName => "秋水";
 
     //Return a string representing your preferred contact details.
-    public override string AuthorContact => "";
+    public override string AuthorContact => "1123993881@qq.com";
 }
 
 partial class SimpleAssemblyPriority
